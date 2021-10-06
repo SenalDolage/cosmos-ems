d6ffba3ab31535feb662aaaec536bfbf2904f6ce
Senal <senal.dolage@gmail.com>
2021-10-06 23:29:15 +0530

    common header and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
